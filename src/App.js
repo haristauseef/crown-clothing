1da1efd7a8d3fa9a5960535a8db47f268060b0f4
haristauseef <haristauseef@gmail.com>
2020-07-08 14:05:30 +0500

    with the collection item component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from "./pages/shop/shop.component";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <React.Fragment>
       <Switch>
       <Route path="/" exact component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
       </Switch>
     </React.Fragment>
   );
