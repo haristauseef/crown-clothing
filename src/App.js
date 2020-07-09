04325eba58d042ff9bfa4b92ad5c328e0e1dde78
haristauseef <haristauseef@gmail.com>
2020-07-09 11:01:57 +0500

    add assets and header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
       <Switch>
       <Route path="/" exact component={HomePage} />
       <Route path="/shop" component={ShopPage} />
