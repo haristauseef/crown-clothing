4bee7cbf7210a7317ab14b70270eea9d4c45513b
haristauseef <haristauseef@gmail.com>
2020-07-21 11:39:00 +0500

    created drop down menu still display is not ok

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = props => {
+const CartDropdown = ({cartItems}) => {
     return(
         <div className="cart-dropdown">
-            <div className="cart-item"></div>
+            <div className="cart-item">
+                {cartItems.map(cartItem => 
+                    <CartItem key={cartItem.id} item={cartItem} />
+                )
+                }
+            </div>
             <CustomButton className="button">Go To Checkout</CustomButton>
         </div>
     )
 }
 
-export default CartDropdown;
+const mapStatetoProps = state => ({
+    cartItems: state.cart.cartItems
+})
+
+export default connect(mapStatetoProps)(CartDropdown);
